b43b3216b9ef052a76974941f5616a7e20e10863
Pradyumn <149019450+pradyumn12211@users.noreply.github.com>
2024-06-20 02:27:34 -0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,9 +12,9 @@ const ExpenseItem = (props) => {
         });
     };
 
-    const increaseAllocation = (name) => {
+    const increaseAllocation = () => {
         const expense = {
-            name: name,
+            name: props.name,
             cost: 10,
         };
 
@@ -22,18 +22,31 @@ const ExpenseItem = (props) => {
             type: 'ADD_EXPENSE',
             payload: expense
         });
+    };
 
-    }
+    const decreaseAllocation = () => {
+        const expense = {
+            name: props.name,
+            cost: -10, // Decrease by 10
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    };
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>£{props.cost}</td>
+            <td>
+                <button onClick={increaseAllocation}>+</button>
+                <button onClick={decreaseAllocation}>-</button> {/* Decrease button */}
+            </td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
 
 export default ExpenseItem;
-
