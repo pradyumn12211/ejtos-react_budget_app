0f8dc4e6ad50c0f9b679b39431622a2f0c5c1dc5
Pradyumn <149019450+pradyumn12211@users.noreply.github.com>
2024-06-20 02:28:09 -0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,41 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, remaining, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const [currency, setCurrency] = useState('$'); 
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        setNewBudget(parseInt(event.target.value));
+    };
+
+    const increaseBudget = () => {
+        const updatedBudget = newBudget + 10;
+        if (updatedBudget <= remaining) {
+            setNewBudget(updatedBudget);
+            dispatch({ type: 'SET_BUDGET', payload: updatedBudget });
+        }
+    };
+
+    const decreaseBudget = () => {
+        const updatedBudget = newBudget - 10;
+        if (updatedBudget >= 0) {
+            setNewBudget(updatedBudget);
+            dispatch({ type: 'SET_BUDGET', payload: updatedBudget });
+        }
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div>
+            <div className='alert alert-secondary'>
+                {/* Currency prefix */}
+                <span className="input-group-text">{currency}</span>
+                <input type="number" value={newBudget} onChange={handleBudgetChange} />
+                <button onClick={increaseBudget}>Increase by 10</button>
+                <button onClick={decreaseBudget}>Decrease by 10</button>
+            </div>
+        </div>
     );
 };
+
 export default Budget;
